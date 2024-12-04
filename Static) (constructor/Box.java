3fa7bc1200338b58a7c58c1f0910070ecbe0fa99
PratikSalunkhe <98522127+pratiksalunkhecode@users.noreply.github.com>
2024-12-04 12:40:39 +0530

    Add files via upload

---
+++ Static) (constructor/Box.java
@@ -0,0 +1,48 @@
+/**
+ * workRevise
+ */
+
+public class Box {
+
+    double l;
+    double h;
+    double w;
+    double weight = 1;
+
+    static void greeting(){
+        System.out.println("hey im in a box class. greeting");
+     }
+
+    Box(){
+         // super(); ref immidiate parent class // to call the contructor // obect class is baap of all the class
+          this.l=-1;
+          this.h=-1;
+          this.w=-1;
+    }
+
+    Box (double side){
+            
+              this.l=side;
+              this.h=side;
+              this.w=side;
+    }
+
+    Box(double l , double h, double w){
+        this.l=l;
+        this.h=h;
+        this.w=w;
+    }
+
+    Box(Box work){
+        this.l=work.l;
+        this.h=work.h;
+        this.w=work.w;
+    }
+
+
+    public void information(){
+        System.out.println("Running the box");
+    }
+
+
+}
